3080e25aded4dd6b43fce92af97ca25cf38003a7
YellowJacketLinux <anymouseprophet@gmail.com>
2024-10-15 20:06:09 -0700

    bug fix

--- 03-libgpg-error.sh
+++ 03-libgpg-error.sh
@@ -6,7 +6,7 @@ if [ "`whoami`" == "root" ]; then
   exit 1
 fi
 
-TARBALL=""
+TARBALL="libgpg-error-1.50.tar.bz2"
 DNL="https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.50.tar.bz2"
 SHA256="69405349e0a633e444a28c5b35ce8f14484684518a508dc48a089992fe93e20a"
 
